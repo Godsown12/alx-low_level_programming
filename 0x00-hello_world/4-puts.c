a6945c9ca34f4d9866c160975ae8a76682403aba
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-14 05:29:22 -0700

    checking

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Write a C program that prints this.
+ * my sweet code
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
