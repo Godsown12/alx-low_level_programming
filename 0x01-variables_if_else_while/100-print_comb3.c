9092f3ec5fcda1f2025054fbe5d3bae9ffe94843
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-14 21:16:33 -0700

    pusing all code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all possible diff
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
