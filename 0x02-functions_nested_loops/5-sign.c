1921c7d543c3762a28c0da537327d85711c29ab9
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-19 08:05:24 -0700

    pushing 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - To display the sign of a number.
+ * @n: number that has the sign value.
+ *
+ * Return: 1 and display plus if n > zero
+ * 0 and display 0 if n = zero
+ * -1 and disply minus if n < zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+
+	_putchar('-');
+	return (-1);
+}
