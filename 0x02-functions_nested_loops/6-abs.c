826a2945171a35313cd101ad0adde3df3aa99668
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-19 10:59:49 -0700

    pushing 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of a number.
+ * @n: This is the variable of the number.
+ * Return: (n).
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
