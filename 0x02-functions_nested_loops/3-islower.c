1a159c4b8c5b3f8f735139afe275bbc6a9d68470
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-19 07:09:54 -0700

    pushing 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checking for lowercase
+ * letters
+ * @c: letter for checking.
+ * Return: 1 if condition is true else 0
+ */
+int _islower(int c)
+{
+
+	if (_islower(c))
+		return (1);
+	else
+		return (0);
+}
