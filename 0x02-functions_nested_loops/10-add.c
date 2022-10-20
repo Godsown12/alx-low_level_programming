b1d856fdd098a372306366e9899a3e03d968f9a4
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-19 17:02:37 -0700

    pushing 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adding two numbers.
+ * @num1: 1st number to be added.
+ * @num2: 2nd number to be added.
+ * Return: sum.
+ */
+int add(int num1, int num2)
+{
+	sum = num1 + num2;
+	return (sum);
+}
