a3e2f833d4e822235df547aaaf8b923e9f82e1e9
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-21 06:42:38 -0700

    pushing 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_numbers - function to print 1-9
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
