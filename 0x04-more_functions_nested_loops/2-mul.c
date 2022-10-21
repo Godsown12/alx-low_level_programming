a42bb4b9986e8483aa91863a544cfabb8cdc2471
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-21 06:31:14 -0700

    pushing 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - function that times two numbers
+* @a: first number
+* @b: second number
+* Return: product of a and b
+*/
+int mul(int a, int b)
+{
+	product = 0;
+	product = a * b;
+
+	return (product);
+}
