1e5efad00abd1f6e09391b9289cee7d56bd05faf
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-21 08:59:08 -0700

    pushing 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - display 1=100.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", i);
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
