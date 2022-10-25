1cad97230358a4a7322492b208423aba8a2cb8a0
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-10-25 08:59:18 -0700

    pushing 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - getting the length of the string
+ * @s: string to check
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s[i];)
+		i++;
+
+	return (i);
+}
