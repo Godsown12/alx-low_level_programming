344e1214185c5b9dbd86dedcdd9c82f491a40588
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-11-01 17:22:50 -0700

    pushing again

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,18 +9,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i;
-	if (*needle == 0)
-		return (haystack);
-	for (; *haystack; haystack++)
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
 	{
-	i = 0;
-	if (haystack[i] == needle[i])
-		do {
-			if (needle[i + 1] == '\0')
-				return (haystack);
-				i++;
-		} while (haystack[i] == needle[i]);
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-	return ('\0');
+	return (0);
 }
