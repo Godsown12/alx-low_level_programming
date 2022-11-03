946218eecf84fa6c3d9d7a64ba7bb7f2a83918f5
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-11-02 17:46:57 -0700

    pushing 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * _sqrt_recursion - function that returns the
+ * square root of a number.
+ * @n: base
+ * Return: n,-1,1.
+ */
+
+int _sqrt_recursion(int n)
+{
+	int q = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n > 0)
+	{
+	n = _sqrt_recursion(q);
+	}
+	return (n);
+}
