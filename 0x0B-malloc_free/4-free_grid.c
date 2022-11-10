7f059b6fac8b9ac74c3c0f6ba4cdfec3f71ea9e8
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-11-09 18:16:14 -0800

    pushing 4-free_grid.c'

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - function that frees a 2 dimensional of numners.
+ * @grid: grid
+ * @height: The height grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
