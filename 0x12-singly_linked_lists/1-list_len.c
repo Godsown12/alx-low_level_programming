19438c02f68511fd6f77221c4e1ac7a93e0b4037
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-11-25 11:41:35 -0800

    pushing 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,21 @@
-
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
+
 /**
- * print_list - A function that prints all elements in
- * list_t list.
- * @h: The pointer to the head of the list.
- * Return: return the number of nodes.
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the i.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-size_t node = 0;
-while (h)
-{
-if (h->str == NULL)
-	printf("[0] (nil)\n");
-else
-	printf("[%d] %s\n", h->len, h->str);
-node++;
-h = h->next;
-}
-return (node);
+	int i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
