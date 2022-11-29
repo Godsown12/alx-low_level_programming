ec30075ef71d241ff58e8ee5150513e3412e2b65
Ajodo Godsown Kole <ajogodsown@gmail.com>
2022-11-29 15:00:37 -0800

    pushing 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint -  prints all the elements of a list.
+ * @h: pointer to the head of the structure.
+ * Return: The number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+MY 0x13. C - More singly linked lists
